1c3db6b23f19f290a8d2be020644267faf51fa19
ManikantaMandal <mandala.manikanta.20cse@bmu.edu.in>
2023-11-17 16:44:24 +0530

    Learn't about Singleton Design Pattern

---
+++ src/main/java/com/DesignPatterns/Main.java
@@ -0,0 +1,14 @@
+package com.DesignPatterns;
+import com.DesignPatterns.Builder.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = Person.builder()
+                .name("Manikanta")
+                .age(21)
+                .emailId("mandala.mallik@gmail.com")
+                .phoneNumber("+91-8688014820")
+                .build();
+        System.out.println(p1.toString());
+    }
+}
